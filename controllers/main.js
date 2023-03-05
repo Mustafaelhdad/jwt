40bf44a0d15cf867b5f0081521757d2f0de6c948
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-03-05 23:06:09 +0200

    working with the jwt for verifing users

--- controllers/main.js
+++ controllers/main.js
@@ -1,4 +1,5 @@
-const CustomAPIErro = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -8,15 +9,39 @@ const login = async (req, res) => {
   // check in the controller
 
   if (!username || !password) {
-    throw new CustomAPIErro();
+    throw new CustomAPIError("Please provide email and apssword", 400);
   }
+
+  // just for demo normally provided by DB!!
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 
   console.log(username, password);
 
-  res.send(`Fake Login/Register/Signup Route`);
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
+  // console.log(req.headers);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
+
   const luckyNumber = Math.floor(Math.random() * 100);
 
   res.status(200).json({
